92b36a24c0663686bea523ba7457e7cdb1aa500c
Argha Koner <arghakoner93@gmail.com>
2025-12-05 23:03:29 +0530

    Assignments 1 - Data Types

---
+++ src/language/basics/HelloWorld.java
@@ -0,0 +1,14 @@
+package language.basics;
+
+
+public class HelloWorld
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello world");
+		System.out.printf("Hello %s", "Argha");
+
+	}
+
+}
